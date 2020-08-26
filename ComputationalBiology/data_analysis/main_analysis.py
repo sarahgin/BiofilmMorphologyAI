c63f31d8c89504a541f199599d4d955f47bc813c
cohenoa <saragin264>
2020-08-26 23:15:23 +0300

    Adding more anaysis and data

--- ComputationalBiology/data_analysis/main_analysis.py
+++ ComputationalBiology/data_analysis/main_analysis.py
@@ -32,10 +32,21 @@ if __name__ == '__main__':
     plt.title('Length histogram of CDS genes')
     plt.show(fig)
 
+    # GC:
+    x = df_cds['GC_CONTENT'].values
+    fig = sns.distplot(x)
+    plt.xlabel('GC_CONTENT')
+    plt.ylabel('counts')
+    plt.title('GC_CONTENT histogram of CDS genes')
+    plt.show(fig)
+
     # scatter plot:
     # NOTE: I EXPECT A POSITIVE CORRELATION!
     fig2 = sns.jointplot(x='MELTING_POINT', y='GC_CONTENT', data=df_cds)
     plt.show(fig2)
+
+    # report genes with melting point > 10,000
+    print(df_cds[df_cds['MELTING_POINT'] > 10000][['GENE_NAME', 'PRODUCT_DESCRIPTION']])
     print('Done main_analysis')
 
 
