a71d818b54ab05e5348a8487907f530ca11e956d
sophiko <sarahgin@gmail.com>
2020-09-02 10:04:12 +0300

    main analysis added hist and save to file

--- ComputationalBiology/data_analysis/main_analysis.py
+++ ComputationalBiology/data_analysis/main_analysis.py
@@ -1,12 +1,15 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import numpy as np
 from numpy import NaN
 from pandas import DataFrame
+import csv
 
 from ComputationalBiology.data_analysis.all_features_calculator import GeneFeatures, ProteinFeatures
 
 FEATURES_DF_FILE = '../../data/dataframes/features_BS3610.pickle'
+#FEATURES_DF_FILE = '../../data/dataframes/features_AL590842_1_EColi.pickle'
 
 
 def get_df_by_product(df: pd.DataFrame, product_type: str):
@@ -18,7 +21,11 @@ def plot_hist_feature(df, feature_name: str, out_file=''):
     # filtering out non valid values
     col = df[feature_name]
     x = col[~col.isnull()].values
-    ig = sns.distplot(x)
+
+    fig = plt.figure()
+    col[~col.isnull()].plot.hist(bins=100)
+
+    #fig = sns.distplot(x)
     plt.xlabel(feature_name)
     plt.ylabel('counts')
     plt.title(feature_name)
@@ -33,7 +40,7 @@ def plot_all_hist_features(df: DataFrame):
     for gene_feature in list(GeneFeatures.__members__.keys()) + list(ProteinFeatures.__members__.keys()):
         assert(gene_feature in df.columns)  # Assuming that the column exists
         print(gene_feature)
-        plot_hist_feature(df, gene_feature, out_file='')  # 'path//{}.png'.format(gene_feature.name)
+        plot_hist_feature(df, gene_feature, out_file='../../data/histograms/{}.png'.format(gene_feature))
 
 
 if __name__ == '__main__':
@@ -55,11 +62,24 @@ if __name__ == '__main__':
     # probably a missing data:
     # df_cds[df_cds['HYDROPHOBIC_AA'].isnull()].iloc[0]
 
-    # plot_all_hist_features(df_cds)
+    #plot_all_hist_features(df_cds)
 
     # get only numerical
+    print(len(df_cds))
+    df_cds = df_cds.replace(to_replace='None', value=np.nan).dropna()
+    print(len(df_cds))
+
     table = df_cds.corr()
     print(table)
+
+    #save correlations to csv file
+    #table.to_csv('corr.csv', index=False)
+
+    #plot heatmap of correlations
+    fig = plt.figure()
+    sns.heatmap(table, annot=True)
+    plt.show()
+
     print('done')
     exit(1)
 
