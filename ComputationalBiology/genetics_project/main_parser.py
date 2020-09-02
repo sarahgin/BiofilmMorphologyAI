4283b880918d614412186cb1389356d08c933232
sophiko <sarahgin@gmail.com>
2020-09-02 10:04:38 +0300

    added ecoli macro

--- ComputationalBiology/genetics_project/main_parser.py
+++ ComputationalBiology/genetics_project/main_parser.py
@@ -7,6 +7,8 @@ from ComputationalBiology.file_utils import genbank_parser
 
 if __name__ == '__main__':
     genbank_file = '../../data/GeneticData/BS3610.gb'
+    #genbank_file = '../../data/GeneticData/AL590842.1_EColi.gb'
+
     assert(os.path.exists(genbank_file))  # making sure that the path is valid
     record_gb = genbank_parser.read_genbank_file(genbank_file)
     spp = genbank_parser.init_species(record_gb)
