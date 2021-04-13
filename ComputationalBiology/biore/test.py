8fbbae548350816ec11dc2d245f57205b2467fd2
sophiko <sarahgin@gmail.com>
2021-04-13 14:40:41 +0300

    biore tweaks

--- ComputationalBiology/biore/test.py
+++ ComputationalBiology/biore/test.py
@@ -1,29 +1,17 @@
 from ComputationalBiology.biore import biore
 import re
 import pandas as pd
+import numpy as np
 
 pd.set_option('display.max_rows', None)
-import matplotlib.pyplot as plt
-from matplotlib_venn import venn2, venn3
-from collections import Counter
+pd.set_option('display.max_columns', None)
+pd.set_option('max_colwidth', 8000)
 from sklearn.metrics import confusion_matrix
-
 from ComputationalBiology.biore.biore_macros import PTS, pts_to_run
 
 if __name__ == '__main__':
-    # reg = '(NP)*?\K'
-    # seq = 'YDREKDREKKDREKDREKK'
-
-    # reg = '\A[PO]{2,5}\Q*?\K\K\K'
-    # tr_reg = biore.translate(reg)
-    # print(tr_reg)
-    # seq = 'ARRKVKKQQQQQQKKKQQQQQKKK'
-
-    # ans = biore.finditer(reg, seq)
-    # lst = [x for x in ans]
-    # print(lst)
-
     if True:
+        #compute matches for all regexes
         df = pd.read_csv('./data/human1.tab', sep='\t', skiprows=0)
         print('Total proteins', len(df))
         df['Sequence'] = df['Sequence'].apply(lambda x: x.upper())
@@ -31,23 +19,34 @@ if __name__ == '__main__':
             signal_name = reg.name
             signal_regex = reg.value
             df[signal_name] = df['Sequence'].apply(lambda x: biore.search(signal_regex, x) is not None)
+            df[signal_name + '_MATCH'] = df['Sequence'].apply(lambda x: str(biore.search(signal_regex, x)))
 
+        # removing all rows where no label exists (empty string)
         df = df.fillna('')
-        df['isNucleus'] = df['Subcellular location [CC]'].apply(lambda x: re.search(r'\bNucleus\b', x) is not None)
-        df['isMitochondria'] = df['Subcellular location [CC]'].apply(
-            lambda x: re.search(r'\bMitochondrion\b', x) is not None)
-        df['isER'] = df['Subcellular location [CC]'].apply(
-            lambda x: re.search(r'\bEndoplasmic reticulum\b', x) is not None)
+        print('total rows: ', len(df))
+        df['Subcellular location [CC]'].replace('', np.nan, inplace=True)
+        df.dropna(subset=['Subcellular location [CC]'], inplace=True)
+        print('after removal of rows with no label:', len(df))
 
-        print(sum(df['isNucleus'] == True))
-        print(sum(df['isMitochondria'] == True))
-        print(sum(df['isER'] == True))
+        # FILL GROUND TRUTH
+        df['isNucleus'] = df['Subcellular location [CC]'].apply(
+            # lambda x: re.search(r'\bNucleus\b', x) is not None)
+            lambda x: re.search(r'^SUBCELLULAR LOCATION: Nucleus', x) is not None)
+        df['isMitochondria'] = df['Subcellular location [CC]'].apply(
+            lambda x: re.search(r'^SUBCELLULAR LOCATION: Mitochondrion', x) is not None)
+        df['isER'] = df['Subcellular location [CC]'].apply(
+            lambda x: re.search(r'^SUBCELLULAR LOCATION: Endoplasmic reticulum', x) is not None)
+        df['isHelix'] = df['Helix'].apply(lambda x: x is not '') #check which helix! alpha/pi
+
+        print('Total nucleus found: ', sum(df['isNucleus'] == True))
+        print('Total mitochondria found: ', sum(df['isMitochondria'] == True))
+        print('Total ER found: ', sum(df['isER'] == True))
 
         for reg in pts_to_run:
             conf = confusion_matrix(df['isNucleus'], df[reg.name])
             tn, fp, fn, tp = conf.ravel()
-            sensitivity = tp/(tp+fn)
-            specificity = tn/(tn+fp)
+            sensitivity = tp / (tp + fn)
+            specificity = tn / (tn + fp)
             print(reg.name, ': ', conf, 'sensitivity: ', sensitivity, 'specificity: ', specificity)
             print('--------------------')
 
@@ -55,9 +54,10 @@ if __name__ == '__main__':
 
     # PRINT LIST OF GENES
     if True:
-        df_exact = pd.read_pickle('./data/human1.pickle')
-        for reg in pts_to_run:
-            num_found = len(df_exact[df_exact[reg.name] == True])
-            print(reg.name, num_found)
+        df = pd.read_pickle('./data/human1.pickle')
+        df.to_csv('./data/test.csv')
+        # for reg in pts_to_run:
+        #    found = df_exact[df_exact[reg.name] == True]
+        #    print(found)
 
     print('done')
