ffcb360c4ff0c1e8fe898d0a32955157ae9f287f
sophiko <sarahgin@gmail.com>
2021-04-20 10:42:15 +0300

    added group_to_aa and aa_to_group functions

--- ComputationalBiology/biore/biore_utils.py
+++ ComputationalBiology/biore/biore_utils.py
@@ -1,4 +1,4 @@
-from ComputationalBiology.biore.biore_macros import AA_GROUP, group_to_amino_acid_dict, aa_group_list
+from ComputationalBiology.biore.biore_macros import AA_GROUP, group_to_aa_dict, aa_group_list, aa_to_group_dict
 import re
 
 
@@ -8,7 +8,7 @@ def translate(original_str: str):
     while i < len(original_str):
         c = original_str[i]
         if c == "\\":
-            res += original_str[i+1]
+            res += original_str[i + 1]
             i += 1
         elif c in aa_group_list:
             res += group_into_aa_or(c)
@@ -20,9 +20,24 @@ def translate(original_str: str):
 
 def group_into_aa_or(group_letter: str):
     # TODO: handle case of invalid group letter
-    relevant_aa = group_to_amino_acid_dict[group_letter]
+    relevant_aa = group_to_aa_dict[group_letter]
     res = '('
     for i in range(len(relevant_aa) - 1):
         res += relevant_aa[i] + '|'
     res += relevant_aa[i + 1] + ')'
+    return res
+
+
+# simple translations single-letter aa into amino acid group letter
+def aa_into_group(seq):
+    res = ''
+    for c in seq:
+        res += aa_to_group_dict[c]
+    return res
+
+
+def group_into_aa(seq):
+    res = ''
+    for c in seq:
+        res += group_to_aa_dict[c]
     return res
