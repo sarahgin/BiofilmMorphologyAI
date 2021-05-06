425685410c3dc13812af2cecfc26e001fc47ae74
FinalProject <cohenoa@gmail.com>
2021-05-06 20:50:49 +0300

    adding PCA graph

--- ComputationalBiology/data_analysis/main_visualization.py
+++ ComputationalBiology/data_analysis/main_visualization.py
@@ -9,6 +9,10 @@ from ComputationalBiology.data_analysis.all_features_calculator import GeneFeatu
 from ComputationalBiology.file_utils.io_utils import create_dir_if_not_exists
 from ComputationalBiology.data_analysis.main_parser_features_calc import species_name, FEATURES_DF_FILE
 
+from sklearn.decomposition import PCA
+# compute PCA on df_joined
+from sklearn.preprocessing import StandardScaler
+
 
 def plot_hist_feature(df, feature_name: str, out_file='', show=False):
     # filtering out non valid values
@@ -49,33 +53,106 @@ def plot_all_features_heatmap(df, show=False, suffix=''):
     create_dir_if_not_exists(out_file)
     fig.savefig(out_file)
 
+
+def plot_pca(df_joined):
+
+    features =\
+        ['GC_CONTENT',
+     'DNA_LENGTH',
+     'HYDROPHOBIC_AA',
+     'HYDROPHILIC_AA',
+     'POLAR_AA',
+     'AROMATIC_AA',
+     'POSITIVE_AA',
+     'NEGATIVE_AA',
+     'NONPOLAR_AA',
+     'AA_LENGTH',
+     'H1',
+     'H2',
+     'H3',
+     'V',
+     'P1',
+     'P2',
+     'SASA',
+     'NCI',
+     'MASS'
+     ]
+
+    # Separating out the features
+    df_joined = df_joined.dropna(0) # NOTE: in BS3610 we had 58 genes of NAN in some of the features
+    x = df_joined.loc[:, features].values
+
+    # Separating out the target
+    y = df_joined.loc[:, ['is_gene_of_interest']].values
+
+    # Standardizing the features
+    x = StandardScaler().fit_transform(x)
+
+    pca = PCA(n_components=2)
+    principalComponents = pca.fit_transform(x, )
+    principalDf = pd.DataFrame(data=principalComponents, columns=['principal component 1', 'principal component 2'])
+
+    finalDf = pd.concat([principalDf, df_joined[['is_gene_of_interest']]], axis=1)
+
+    fig = plt.figure(figsize=(8, 8))
+    ax = fig.add_subplot(1, 1, 1)
+    ax.set_xlabel('Principal Component 1', fontsize=15)
+    ax.set_ylabel('Principal Component 2', fontsize=15)
+    ax.set_title('2 component PCA', fontsize=20)
+    # targets = [False, True]
+    targets = set(finalDf['is_gene_of_interest'].values)
+    colors = ['b', 'r']
+    for target, color in zip(targets, colors):
+        indicesToKeep = finalDf['is_gene_of_interest'] == target
+        ax.scatter(finalDf.loc[indicesToKeep, 'principal component 1'],
+                   finalDf.loc[indicesToKeep, 'principal component 2'],
+                   c=color, s=50)
+    ax.legend(targets)
+    ax.grid()
+    plt.show()
+    print('done')
+
+
 #FOR POSTER GRAPHS ONLY
 if __name__ == '__main__':
-    FEATURES_DF_FILE_BS168 = '../../data/data_outputs/features_BS168.pickle'
-    df_all_BS168 = pd.read_pickle(FEATURES_DF_FILE_BS168)
+    # FEATURES_DF_FILE = '../../data/data_outputs/features_BS168.pickle'
+    # FEATURES_DF_FILE
+    df_all_BS168 = pd.read_pickle(FEATURES_DF_FILE)
+    print(FEATURES_DF_FILE)
     df_cds_BS168 = df_all_BS168[df_all_BS168['PRODUCT_TYPE'] == 'CDS'].copy()
     # s1 = set(df_cds_BS168['GENE_NAME'])
     # change to lower in order to join with the uniprot table
     df_cds_BS168['GENE_NAME'] = df_cds_BS168['GENE_NAME'].apply(lambda x: x.lower())
 
-    # UNIPROT DF:
-    UNIPROT_FILE = '../../data/data_inputs/bs168_uniprot.tab'
-    df_UP = pd.read_csv(UNIPROT_FILE, sep='\t', header=0)
-    df_UP = df_UP.fillna('')
-    df_UP['isSecreted'] = df_UP['Subcellular location [CC]'].apply(lambda x: re.search('Secreted', x) is not None)
-    df_UP['isExtracellular'] = df_UP['Topological domain'].apply(lambda x: re.search('Extracellular', x) is not None)
-    # Note: we fetched only the 1st name in the list
-    df_UP['GENE_NAME'] = df_UP['Gene names'].apply(lambda x: x.lower().split()[0] if x != '' else 'NA_GENE_NAME_UNIPROT')
+    use_uniprot = False
+    if use_uniprot:
+        # UNIPROT DF:
+        UNIPROT_FILE = '../../data/data_inputs/bs168_uniprot.tab'
+        df_UP = pd.read_csv(UNIPROT_FILE, sep='\t', header=0)
+        df_UP = df_UP.fillna('')
+        df_UP['isSecreted'] = df_UP['Subcellular location [CC]'].apply(lambda x: re.search('Secreted', x) is not None)
+        df_UP['isExtracellular'] = df_UP['Topological domain'].apply(lambda x: re.search('Extracellular', x) is not None)
+        # Note: we fetched only the 1st name in the list
+        df_UP['GENE_NAME'] = df_UP['Gene names'].apply(lambda x: x.lower().split()[0] if x != '' else 'NA_GENE_NAME_UNIPROT')
 
-    df_joined = pd.merge(df_cds_BS168, df_UP, on="GENE_NAME")
+        df_joined = pd.merge(df_cds_BS168, df_UP, on="GENE_NAME")
 
-    df_genes_interest = df_joined[df_joined['isSecreted'] | df_joined['isExtracellular']]
-    df_other_genes = df_joined[~df_joined['isSecreted'] & ~df_joined['isExtracellular']]
+        df_joined['is_gene_of_interest'] = df_joined['isSecreted'] | df_joined['isExtracellular']
+        df_genes_interest = df_joined[df_joined['isSecreted'] | df_joined['isExtracellular']]
+        df_other_genes = df_joined[~df_joined['isSecreted'] & ~df_joined['isExtracellular']]
+    else:
+        df_joined = df_cds_BS168
+        df_joined = df_joined.reset_index()
 
-    plot_all_features_histograms(df_genes_interest, suffix='_Secreted_Extracellular')
-    plot_all_features_histograms(df_other_genes, suffix='_Other')
-    plot_all_features_heatmap(df_genes_interest, suffix='_Secreted_Extracellular')
-    plot_all_features_heatmap(df_other_genes, suffix='_Other')
+    # plot_all_features_histograms(df_genes_interest, suffix='_Secreted_Extracellular')
+    # plot_all_features_histograms(df_other_genes, suffix='_Other')
+    # plot_all_features_heatmap(df_genes_interest, suffix='_Secreted_Extracellular')
+    # plot_all_features_heatmap(df_other_genes, suffix='_Other')
+
+    # set all labels to true just in order to have a single color
+
+    df_joined['is_gene_of_interest'] = True
+    plot_pca(df_joined)
 
     print('done')
     # sres = set(s1_lower).intersection(set(s2_lower))
